5726d9069459d60c4439b0c2d19befb30ff55ec6
Donesrom <76392098+Donesrom@users.noreply.github.com>
2022-03-04 09:26:06 +0300

    Delete blog-project/BLOG directory

--- blog-project/BLOG/First/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import *
-
-# Create your views here.
-
-def index(request):
-    psts=Posts.objects.all()
-    context = {'psts':psts}
-    return render(request, "index.html",context)
